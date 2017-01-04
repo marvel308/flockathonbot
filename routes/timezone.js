de27c878a4b7f7eade94e435022072d345fc6b23
vaibhav.tr <vaibhav.tr@directi.com>
2017-01-04 11:41:25 +0530

    checking for bot

--- routes/timezone.js
+++ routes/timezone.js
@@ -7,11 +7,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    request('http://www.convert-unix-time.com/api?date=2011-02-01%2008:23:33&timezone=india', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.send(body) // Show the HTML for the Google homepage.
-        }
-    })
+    res.send(JSON.stringify(req.params));
+    // request('http://www.convert-unix-time.com/api?date=2011-02-01%2008:23:33&timezone=india', function (error, response, body) {
+    //     if (!error && response.statusCode == 200) {
+    //         res.send(body) // Show the HTML for the Google homepage.
+    //     }
+    // })
 
 });
 
