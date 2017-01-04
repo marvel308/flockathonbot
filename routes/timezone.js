d18b5080cd7f48756b16b0a67afd9a00120f37eb
vaibhav.tr <vaibhav.tr@directi.com>
2017-01-04 11:43:52 +0530

    changed to originalUrl

--- routes/timezone.js
+++ routes/timezone.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send(JSON.stringify(req.params));
+    res.send(JSON.stringify(req.originalUrl));
     // request('http://www.convert-unix-time.com/api?date=2011-02-01%2008:23:33&timezone=india', function (error, response, body) {
     //     if (!error && response.statusCode == 200) {
     //         res.send(body) // Show the HTML for the Google homepage.
